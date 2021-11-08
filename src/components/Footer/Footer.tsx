832e2aa9e4a320119073f5aaef3a18b3150e0de3
MarioDolci9 <mario.dolci@everis.nttdata.com>
2021-11-08 15:26:03 +0100

    Adding header and footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,69 @@
-import { Box } from '@mui/system';
-import { Typography } from '@mui/material';
+import { Box, Grid, Link, SvgIcon, Typography } from '@mui/material';
+import { ReactComponent as logo } from '../../assets/logo_pago_pa.svg';
 
+const Footer = () => (
+  <Box
+    component="footer"
+    sx={{
+      pt: '32px',
+      mt: 'auto',
+      height: '156px',
+      bgcolor: '#01254C',
+      alignItems: 'center',
+    }}
+  >
+    <Grid container justifyContent={'center'} alignItems={'center'}>
+      <Box sx={{ width: '90%', display: 'flex' }}>
+        <SvgIcon component={logo} viewBox="0 0 120 33" sx={{ width: '119px' }} />
+        <Box sx={{ textAlign: 'end', flexGrow: 1 }}>
+          <Typography
+            component="div"
+            sx={{
+              fontWeight: 'normal',
+              fontSize: '15px',
+              lineHeight: '22,82px',
+              textAlign: 'left',
+              color: 'background.default',
+              paddingLeft: '30px',
+            }}
+          >
+            PagoPA S.p.A. - società per azioni con socio unico - capitale sociale di euro 1,000,000
+            interamente versato - sede legale in Roma, Piazza Colonna 370, CAP 00187 - n. di
+            iscrizione a Registro Imprese di Roma, CF e P.IVA 15376371009
+          </Typography>
+        </Box>
+      </Box>
+    </Grid>
+    <Grid container alignItems={'center'} justifyContent={'center'}>
+      <Box sx={{ width: '90%', display: 'flex' }}>
+        <Typography
+          style={{
+            fontWeight: 'normal',
+            fontSize: '15px',
+            lineHeight: '15px',
+            textAlign: 'left',
+            padding: '27px 0px',
+          }}
+          component="div"
+        >
+          <Link
+            href="https://www.pagopa.gov.it/it/privacy-policy/"
+            underline="none"
+            sx={{ marginRight: '10px', color: 'text.disabled' }}
+          >
+            {'Privacy Policy '}{' '}
+          </Link>
+          <Link
+            href="https://pagopa.portaleamministrazionetrasparente.it/"
+            underline="none"
+            sx={{ margin: '10px', color: 'text.disabled' }}
+          >
+            {'Società Trasparente '}{' '}
+          </Link>
+        </Typography>
+      </Box>
+    </Grid>
+  </Box>
+);
 
-export function Footer() {
-  return (
-    <Box
-      component="footer"
-      sx={{
-        py: 3,
-        px: 2,
-        
-        backgroundColor: '#01254C',
-        color: 'white',
-      }}
-    >
-      <Typography variant="h6" align="center" gutterBottom>
-        Footer
-      </Typography>
-      <Typography variant="subtitle1" align="center" component="p">
-        Something here to give the footer a purpose!
-      </Typography>
-    </Box>
-  );
-}
+export default Footer;
