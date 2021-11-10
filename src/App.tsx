0107253bc040426f397a714c6ff6e0dd6d028b9d
Elena Signore <92723520+esignore@users.noreply.github.com>
2021-11-10 17:12:44 +0100

    Apply suggestions from code review
    Co-authored-by: antonioT90 <34568575+antonioT90@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import Layout from './components/Layout/Layout';
 import { LoadingOverlay } from './components/Loading/LoadingOverlay';
 import withLogin from './decorators/withLogin';
 import withParties from './decorators/withParties';
-// import PartySelectionContainer from './pages/partySelectionContainer/PartySelectionContainer';
+import PartySelectionContainer from './pages/partySelectionContainer/PartySelectionContainer';
 import { useAppSelector } from './redux/hooks';
 import { appStateSelectors } from './redux/slices/appStateSlice';
-import Dashboard from './pages/dashboard/Dashboard';
 
 const App = () => {
   const loading = useAppSelector(appStateSelectors.selectLoading);
@@ -13,8 +12,7 @@ const App = () => {
   return (
     <Layout>
       {loading && <LoadingOverlay />}
-      {/* <PartySelectionContainer /> */}
-      <Dashboard />
+      <PartySelectionContainer />
     </Layout>
   );
 };
