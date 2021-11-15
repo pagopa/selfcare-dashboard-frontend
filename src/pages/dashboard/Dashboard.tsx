dd8631ed16b44323cff405a0284fcf0636fed70c
USERSAD\esignore <elena.signore@nttdata.com>
2021-11-15 13:14:25 +0100

    modified dashboard grid view

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -41,13 +41,13 @@ export default function Dashboard() {
     setProducts(activeProducts);
   }, []);
   return (
-    <Grid container>
-      <Grid item xs={3}>
+    <Grid container pl={10}>
+      <Grid item xs={2}>
         <Box sx={{ backgroundColor: '#FFFFFF', height: '2148px' }}>
           <DashboardSideMenu />
         </Box>
       </Grid>
-      <Grid item xs={9} px={10} sx={{ backgroundColor: '#E6E9F2' }}>
+      <Grid item xs={10} px={10} sx={{ backgroundColor: '#E6E9F2' }}>
         <Box >
           <WelcomeDashboard />
           <ActiveProductsSection />
