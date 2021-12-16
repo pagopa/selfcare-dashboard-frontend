9005001c33f2691d3c54fc35b6c59eb5604d1b0a
anttorre <antonio.torre@everis.com>
2021-12-16 16:44:20 +0100

    tmp

--- openApi/scripts/fixSwagger20ArraySchemaDef.js
+++ openApi/scripts/fixSwagger20ArraySchemaDef.js
@@ -1,16 +1,24 @@
 const regexReplace = require('regex-replace');
+const fs = require('fs');
 
 console.log(`Fixing Swagger 2.0 schema array definition in ${process.argv[2]}`);
 
+const filePath = process.argv[2];
+const pattern =
+  /"schema"\s*:\s*\{[^{]+\{\s*"\$ref"\s*:\s*"#\/definitions\/([^"]+)"[^,]+,\s+"type"\s*:\s*"array"[^}]+}((?:\n|.)*"definitions"\s*:\s*\{)/gms;
+
 function fixArrayDef() {
-  const pattern =
-    /"schema"\s*:\s*\{[^{]+\{\s*"\$ref"\s*:\s*"#\/definitions\/([^"]+)"[^,]+,\s+"type"\s*:\s*"array"[^}]+}((?:\n|.)*"definitions"\s*:\s*\{)/gms;
   regexReplace(
     pattern,
     '"schema":{"$ref":"#/definitions/$1Array"}$2"$1Array":{"type": "array", "items": {"$ref": "#/definitions/$1"}},',
-    process.argv[2]
+    filePath
   ).then((r) => {
-    if (re) fixArrayDef();
+    fs.readFile(filePath, 'utf8', function (err, doc) {
+      console.log(doc.match('get'));
+      if (doc.match(pattern)) {
+        fixArrayDef();
+      }
+    });
   });
 }
 
