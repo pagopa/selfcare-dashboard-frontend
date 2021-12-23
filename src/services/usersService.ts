3302ba1775e57082bd3076289aa2b9db8567411b
anttorre <antonio.torre@everis.com>
2021-12-23 13:15:49 +0100

    mocking save REST if configured

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -12,6 +12,7 @@ import { ProductRole } from '../model/ProductRole';
 import { DashboardApi } from '../api/DashboardApiClient';
 import {
   fetchPartyUsers as fetchPartyUsersMocked,
+  savePartyUser as savePartyUserMocked,
   mockedProductRoles,
 } from './__mocks__/usersService';
 
@@ -51,7 +52,14 @@ export const savePartyUser = (
   party: Party,
   product: Product,
   user: PartyUserOnCreation
-): Promise<any> => DashboardApi.savePartyUser(party.institutionId, product.id, user);
+): Promise<any> => {
+  /* istanbul ignore if */
+  if (process.env.REACT_APP_API_MOCK_PARTY_USERS === 'true') {
+    return savePartyUserMocked(party, product, user);
+  } else {
+    return DashboardApi.savePartyUser(party.institutionId, product.id, user);
+  }
+};
 
 export const updatePartyUserStatus = (user: PartyUser, status: UserStatus): Promise<any> => {
   if (user.products.length !== 1) {
