90c76cc529af02988f0a6b4e744a68a2102ee264
loremedda <lorenzo.medda.sa@nttdata.com>
2022-03-15 12:14:12 +0100

    fix

--- src/pages/dashboardGroupEdit/CloneGroupPage.tsx
+++ src/pages/dashboardGroupEdit/CloneGroupPage.tsx
@@ -59,11 +59,10 @@ function CloneGroupPage({ party, activeProducts, productsMap, partyGroup }: Prop
               description: partyGroup.description,
               members: partyGroup.members,
               institutionId: partyGroup.institutionId,
-              productId: partyGroup.members.find(
-                (cu) => cu.isCurrentUser && productsMap[partyGroup.productId]?.userRole === 'ADMIN'
-              )
-                ? partyGroup.productId
-                : undefined,
+              productId:
+                productsMap[partyGroup.productId]?.userRole === 'ADMIN'
+                  ? partyGroup.productId
+                  : undefined,
             } as PartyGroupOnEdit
           }
           isClone={true}
