28af6ba82ec6e3322a133107717d4def0037117e
anttorre <antonio.torre@everis.com>
2022-03-24 19:19:08 +0100

    minor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import '@pagopa/selfcare-common-frontend/common-polyfill';
-
-// TODO remove after PR on common
-import 'core-js/es/object/from-entries';
-import 'core-js/es/map';
-
 import('./bootstrap');
