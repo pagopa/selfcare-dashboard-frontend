5fd9e1ef25304a6a1595d9a06ee9c1614f64598e
USERSAD\esignore <elena.signore@nttdata.com>
2022-04-28 12:27:28 +0200

    SELC-890

--- src/pages/dashboardOverview/components/notActiveProductsSection/components/NotActiveProductCard.tsx
+++ src/pages/dashboardOverview/components/notActiveProductsSection/components/NotActiveProductCard.tsx
@@ -14,7 +14,9 @@ type Props = {
 };
 
 const goToOnboarding = (product: Product, party: Party): void =>
-  window.location.assign(`${ENV.URL_FE.ONBOARDING}/${product.id}?partyId=${party.externalId}`);
+  window.location.assign(
+    `${ENV.URL_FE.ONBOARDING}/${product.id}?partyExternalId=${party.externalId}`
+  );
 
 export default function NotActiveProductCard({ party, product }: Props) {
   const { t } = useTranslation();
