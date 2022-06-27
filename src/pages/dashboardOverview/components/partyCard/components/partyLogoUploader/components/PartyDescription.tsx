ecd7db365b2f84e36964f5b8b486221d56f8faae
USERSAD\esignore <elena.signore@nttdata.com>
2022-06-27 16:13:24 +0200

    SELC-1122-Q&A_modified party logo with ButtonNaked

--- src/pages/dashboardOverview/components/partyCard/components/partyLogoUploader/components/PartyDescription.tsx
+++ src/pages/dashboardOverview/components/partyCard/components/partyLogoUploader/components/PartyDescription.tsx
@@ -1,16 +1,14 @@
-import { Grid, Link, Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import { useTranslation, Trans } from 'react-i18next';
 import EditIcon from '@mui/icons-material/Edit';
-import { Box } from '@mui/system';
+import { ButtonNaked } from '@pagopa/mui-italia';
+import { MouseEventHandler } from 'react';
 
 // Utility to wait some time
 
 type Props = {
   labelLink: string;
-  open:
-    | React.MouseEventHandler<HTMLAnchorElement>
-    | React.MouseEventHandler<HTMLSpanElement>
-    | undefined;
+  open: MouseEventHandler<HTMLButtonElement> | undefined;
   loading: boolean;
   files: Array<File>;
 };
@@ -19,21 +17,15 @@ export function PartyDescription({ labelLink, open, loading, files }: Props) {
   const { t } = useTranslation();
   return (
     <Grid container justifyContent={'flex-start'} alignItems={'center'}>
-      {!loading && <EditIcon color={'primary'} fontSize="small" />}
-      <Box display="flex" alignItems="center" ml={1}>
-        <Link
-          color={'primary'}
-          onClick={open}
-          variant="caption"
-          sx={{
-            fontWeight: 'fontWeightMedium',
-            cursor: 'pointer',
-            textDecoration: 'none !important',
-          }}
-        >
-          {files && files.length > 0 && files[0].name ? files[0].name : labelLink}
-        </Link>
-      </Box>
+      <ButtonNaked
+        component="button"
+        onClick={open}
+        startIcon={!loading ? <EditIcon /> : undefined}
+        sx={{ color: 'primary.main' }}
+        weight="default"
+      >
+        {files && files.length > 0 && files[0].name ? files[0].name : labelLink}
+      </ButtonNaked>
       <Typography
         mt={1}
         variant="body2"
