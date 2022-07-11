81fbbd4fd53d6cdc9dab4d4b12ef98fec3dc18a5
loremedda <lorenzo.medda.sa@nttdata.com>
2022-07-11 16:14:42 +0200

    clean active and notActive products components

--- src/pages/dashboardOverview/components/activeProductsSection/components/ActiveProductCard.tsx
+++ src/pages/dashboardOverview/components/activeProductsSection/components/ActiveProductCard.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import {
   Typography,
   Button,
@@ -7,7 +7,8 @@ import {
   CardContent,
   Link,
   Chip,
-  Paper,
+  Card,
+  CardActions,
   useTheme,
   Tooltip,
 } from '@mui/material';
@@ -50,86 +51,77 @@ export default function ActiveProductCard({
     [product.subProducts]
   );
   return (
-    <React.Fragment>
-      <Paper
-        elevation={8}
-        sx={{
-          height: '200px',
-          borderRadius: theme.spacing(2),
-        }}
-      >
-        <CardContent sx={{ height: '100%' }}>
-          <Grid container sx={{ height: '100%' }}>
-            <Grid item xs={12} display="flex" alignItems="center">
-              <Box display="flex">
+    <Card
+      raised
+      sx={{
+        height: '200px',
+      }}
+    >
+      <CardContent sx={{ height: '100%' }}>
+        <Grid container sx={{ height: '100%' }}>
+          <Grid item xs={12} display="flex" alignItems="center">
+            <Box display="flex" flexDirection="column" justifyContent="center" mr={2}>
+              <img src={urlLogo} />
+            </Box>
+            <Box display="flex" flexDirection="column" justifyContent="center">
+              {cardTitle && (
+                <Box display="flex" alignItems={'center'}>
+                  <Tooltip title={cardTitle.length > 27 ? cardTitle : ''}>
+                    <Typography
+                      variant="h6"
+                      sx={{
+                        fontSize: theme.typography.fontSize,
+                        height: '100%',
+                        width: '100%',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        display: '-webkit-box',
+                        WebkitBoxOrient: 'vertical' as const,
+                        WebkitLineClamp: 2,
+                      }}
+                    >
+                      {cardTitle}
+                    </Typography>
+                  </Tooltip>
+                </Box>
+              )}
+              {!disableBtn &&
+                activeSubProducts.map((p) => (
+                  <Chip
+                    key={p.id}
+                    label={p.title}
+                    color="primary"
+                    size="small"
+                    sx={{ borderRadius: theme.shape, mt: 1, width: '80px' }}
+                  />
+                ))}
+            </Box>
+          </Grid>
+          <Grid item xs={12} display="flex">
+            <Box sx={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
+              {disableBtn ? (
                 <Box
                   display="flex"
-                  flexDirection="column"
-                  justifyContent="center"
-                  // sx={{ columnWidth: '88px', height: '88px', textAlign: 'center', pt: '10px' }}
-                  mr={2}
+                  alignItems="center"
+                  justifyContent="start"
+                  sx={{ color: '#5C6F82', cursor: 'pointer' }}
                 >
-                  <img src={urlLogo} />
+                  <Typography sx={{ fontSize: '16px' }}>
+                    <Trans i18nKey="activeProductCard.disableInfo">
+                      Per gestire questo prodotto, chiedi a uno dei suoi
+                      <Link
+                        onClick={() =>
+                          onExit(() => history.push(party.partyId ? usersPath : usersRoute))
+                        }
+                        sx={{ fontWeight: 'fontWeightMedium' }}
+                      >
+                        Amministratori
+                      </Link>
+                    </Trans>
+                  </Typography>
                 </Box>
-                <Box display="flex" flexDirection="column" justifyContent="center">
-                  {cardTitle && (
-                    <Box display="flex" alignItems={'center'}>
-                      <Tooltip title={cardTitle.length > 27 ? cardTitle : ''}>
-                        <Typography
-                          variant="h6"
-                          sx={{
-                            fontSize: theme.typography.fontSize,
-                            height: '100%',
-                            width: '100%',
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            display: '-webkit-box',
-                            WebkitBoxOrient: 'vertical' as const,
-                            WebkitLineClamp: 2,
-                          }}
-                        >
-                          {cardTitle}
-                        </Typography>
-                      </Tooltip>
-                    </Box>
-                  )}
-                  {!disableBtn &&
-                    activeSubProducts.map((p) => (
-                      <Chip
-                        key={p.id}
-                        label={p.title}
-                        color="primary"
-                        size="small"
-                        sx={{ borderRadius: theme.shape, mt: 1, width: '80px' }}
-                      />
-                    ))}
-                </Box>
-              </Box>
-            </Grid>
-            <Grid item xs={12} display="flex" alignItems="flex-end">
-              <Box sx={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
-                {disableBtn ? (
-                  <Box
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="start"
-                    sx={{ color: '#5C6F82', cursor: 'pointer' }}
-                  >
-                    <Typography sx={{ fontSize: '16px' }}>
-                      <Trans i18nKey="activeProductCard.disableInfo">
-                        Per gestire questo prodotto, chiedi a uno dei suoi
-                        <Link
-                          onClick={() =>
-                            onExit(() => history.push(party.partyId ? usersPath : usersRoute))
-                          }
-                          sx={{ fontWeight: 'fontWeightMedium' }}
-                        >
-                          Amministratori
-                        </Link>
-                      </Trans>
-                    </Typography>
-                  </Box>
-                ) : (
+              ) : (
+                <CardActions sx={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
                   <Button
                     onClick={btnAction}
                     disabled={disableBtn}
@@ -138,12 +130,12 @@ export default function ActiveProductCard({
                   >
                     {buttonLabel}
                   </Button>
-                )}
-              </Box>
-            </Grid>
+                </CardActions>
+              )}
+            </Box>
           </Grid>
-        </CardContent>
-      </Paper>
-    </React.Fragment>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 }
