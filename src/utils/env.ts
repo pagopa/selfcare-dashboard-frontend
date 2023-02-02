dc5c3171b98bf3413706f2d87dab5fd7f2a4344f
Elena Signore <92723520+esignore@users.noreply.github.com>
2023-02-02 14:55:48 +0100

    changed geotax path

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -33,7 +33,7 @@ export const ENV = {
   URL_API: {
     API_DASHBOARD: env.get('REACT_APP_URL_API_DASHBOARD').required().asString(),
     // API_GEOTAXONOMY: env.get('REACT_APP_URL_API_GEOTAXONOMY').required().asString(),
-    API_GEOTAXONOMY: 'https://api.dev.selfcare.pagopa.it/external/geotaxonomy',
+    API_GEOTAXONOMY: 'https://api.dev.selfcare.pagopa.it/external/geo-tax/geotaxonomy',
   },
 
   GEOTAXONOMY: {
