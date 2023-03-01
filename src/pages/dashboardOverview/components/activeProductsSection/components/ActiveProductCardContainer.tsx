8dff24b9e2ed0e008bb7a4342da6e57b274215fb
Elena Signore <92723520+esignore@users.noreply.github.com>
2023-03-01 17:14:42 +0100

    Revert "selc-2009 - temporarily revrted old behavior in order to fix production problems" (#447)
    This reverts commit 7db2db8dc4db52ddb1568a06d3996907d0d82ddd.

--- src/pages/dashboardOverview/components/activeProductsSection/components/ActiveProductCardContainer.tsx
+++ src/pages/dashboardOverview/components/activeProductsSection/components/ActiveProductCardContainer.tsx
@@ -1,67 +1,69 @@
-import { Grid, Typography } from '@mui/material';
-import { SessionModal } from '@pagopa/selfcare-common-frontend';
-import { formatDateAsLongString } from '@pagopa/selfcare-common-frontend/utils/utils';
+import { Grid } from '@mui/material';
 import { useState } from 'react';
 import { useTranslation, Trans } from 'react-i18next';
 import { useTokenExchange } from '../../../../../hooks/useTokenExchange';
 import { Party } from '../../../../../model/Party';
 import { Product } from '../../../../../model/Product';
 import ActiveProductCard from './ActiveProductCard';
+import SessionModalTestProduct from './SessionModalTestProduct';
 
 type Props = {
   party: Party;
   product: Product;
+  prodInteropAndProdInteropColl: boolean;
+  products: Array<Product>;
 };
 
-export default function ActiveProductCardContainer({ party, product }: Props) {
+export default function ActiveProductCardContainer({
+  party,
+  product,
+  prodInteropAndProdInteropColl,
+  products,
+}: Props) {
   const { t } = useTranslation();
   const { invokeProductBo } = useTokenExchange();
 
   const [openEnvironmentModal, setOpenEnvironmentModal] = useState<boolean>(false);
 
   const isDisabled = product.authorized === false;
-  const lastServiceActivationDate = undefined; // actually this info is not available
 
   return (
     <>
-      <Grid item xs={6} lg={4} xl={3}>
+      <Grid item xs={6} lg={4}>
         <ActiveProductCard
           disableBtn={isDisabled}
           cardTitle={product.title}
           buttonLabel={t('overview.activeProducts.manageButton')}
           urlLogo={product.logo}
           btnAction={() =>
-            product.backOfficeEnvironmentConfigurations
+            prodInteropAndProdInteropColl && product.id === 'prod-interop'
               ? setOpenEnvironmentModal(true)
               : invokeProductBo(product, party)
           }
           party={party}
           product={product}
         />
-        {lastServiceActivationDate && (
-          <Typography variant="h5" sx={{ fontSize: '16px' }} mx={1}>
-            {t('overview.lastServiceActive') +
-              `${lastServiceActivationDate && formatDateAsLongString(lastServiceActivationDate)}`}
-          </Typography>
-        )}
       </Grid>
-      <SessionModal
+      <SessionModalTestProduct
         open={openEnvironmentModal}
         title={t('overview.activeProducts.activeProductsEnvModal.title')}
         message={
           <Trans i18nKey="overview.activeProducts.activeProductsEnvModal.message">
-            L&apos;ambiente di test ti permette di conoscere
+            Sei stato abilitato ad operare in entrambi gli ambienti. Ti ricordiamo che
+            l&apos;ambiente di collaudo ti permette di conoscere
             <strong>{{ productTitle: product.title }}</strong> e fare prove in tutta sicurezza.
-            L&apos;ambiente di Produzione è il prodotto in esercizio effettivo.
+            L&apos;ambiente di produzione è il prodotto in esercizio.
           </Trans>
         }
         onConfirmLabel={t('overview.activeProducts.activeProductsEnvModal.envProdButton')}
         onCloseLabel={t('overview.activeProducts.activeProductsEnvModal.backButton')}
-        onConfirm={(e) => invokeProductBo(product, party, (e.target as HTMLInputElement).value)}
+        onConfirm={() => invokeProductBo(product, party)}
         handleClose={() => {
           setOpenEnvironmentModal(false);
         }}
-        productEnvironments={product?.backOfficeEnvironmentConfigurations}
+        prodInteropAndProdInteropColl={prodInteropAndProdInteropColl}
+        products={products}
+        party={party}
       />
     </>
   );
