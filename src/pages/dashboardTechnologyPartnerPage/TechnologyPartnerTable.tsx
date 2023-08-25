ace5e23d8f0695b993b490c0d4fa16199bcfd2c9
esignore <elena.signore@nttdata.com>
2023-08-25 16:17:52 +0200

    SELC-2789

--- src/pages/dashboardTechnologyPartnerPage/TechnologyPartnerTable.tsx
+++ src/pages/dashboardTechnologyPartnerPage/TechnologyPartnerTable.tsx
@@ -1,5 +1,6 @@
 import {
   CircularProgress,
+  IconButton,
   Table,
   TableBody,
   TableCell,
@@ -13,12 +14,17 @@ import { Box } from '@mui/system';
 import { useErrorDispatcher } from '@pagopa/selfcare-common-frontend';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import Paper from '@mui/material/Paper';
 import { ButtonNaked } from '@pagopa/mui-italia';
 import { DelegationResource } from '../../api/generated/b4f-dashboard/DelegationResource';
 import { Party } from '../../model/Party';
 import { Product } from '../../model/Product';
-import { fetchTechnologyPartnersList } from '../../services/technologyPartnerService';
+import {
+  fetchTechnologyPartnersList,
+  mockedTechPartner,
+} from '../../services/technologyPartnerService';
 
 type Props = {
   party: Party;
@@ -72,7 +78,7 @@ export default function TechnologyPartnerTable({ party }: Props) {
   const retrievePtList = async () => {
     setLoading(true);
     await fetchTechnologyPartnersList(party.partyId)
-      .then((r) => setTableList(r)) // TODO temporaly modified with mock, add r of then
+      .then(() => setTableList(mockedTechPartner)) // TODO temporaly modified with mock, add r of then
       .catch((reason) => {
         addError({
           id: `FETCH_PARTY_PT_ERROR-${party.partyId}`,
@@ -95,25 +101,78 @@ export default function TechnologyPartnerTable({ party }: Props) {
     }
   }, [filteredArray]);
 
+  //
+
+  const [data, setData] = useState<Array<DelegationResource>>(filteredArray);
+  const [orderBy, setOrderBy] = useState<'institutionName' | 'productId'>('institutionName');
+  const [order, setOrder] = useState<'asc' | 'desc'>('asc');
+
+  useEffect(() => {
+    setData(filteredArray);
+  }, [filteredArray]);
+
+  const handleSort = (field: 'institutionName' | 'productId') => {
+    if (field === orderBy) {
+      setOrder(order === 'asc' ? 'desc' : 'asc');
+    } else {
+      setOrderBy(field);
+      setOrder('asc');
+    }
+  };
+
+  const sortedData = [...data].sort((a: any, b: any) => {
+    const aValue = orderBy === 'institutionName' ? a.institutionName : a.productId;
+    const bValue = orderBy === 'institutionName' ? b.institutionName : b.productId;
+
+    if (order === 'asc') {
+      return aValue?.localeCompare(bValue);
+    } else {
+      return bValue?.localeCompare(aValue);
+    }
+  });
+
   return !loading ? (
     <>
       {filteredArray && filteredArray.length > 0 ? (
-        <Box height={expandMore && areMoreThen5Elemnts ? '100%' : '360px'}>
+        <Box
+          sx={{
+            height:
+              expandMore && areMoreThen5Elemnts ? '100%' : !areMoreThen5Elemnts ? '100%' : '360px',
+          }}
+        >
           <TableContainer component={Paper} sx={{ height: '100%', overflow: 'hidden' }}>
             <Table sx={{ minWidth: 'auto', height: '100%' }} aria-label="simple table">
               <TableHead>
                 <TableRow>
                   <TableCell sortDirection={'asc'}>
                     {t('overview.ptPage.headerPtTableLabels.party')}
+                    <IconButton onClick={() => handleSort('institutionName')}>
+                      {orderBy === 'institutionName' &&
+                        (order === 'asc' ? (
+                          <ArrowUpwardIcon fontSize="small" />
+                        ) : (
+                          <ArrowDownwardIcon fontSize="small" />
+                        ))}
+                    </IconButton>
                   </TableCell>
-                  <TableCell>{t('overview.ptPage.headerPtTableLabels.product')}</TableCell>
+                  <TableCell>
+                    {t('overview.ptPage.headerPtTableLabels.product')}
+                    <IconButton onClick={() => handleSort('productId')}>
+                      {orderBy === 'productId' &&
+                        (order === 'asc' ? (
+                          <ArrowUpwardIcon fontSize="small" />
+                        ) : (
+                          <ArrowDownwardIcon fontSize="small" />
+                        ))}
+                    </IconButton>
+                  </TableCell>
                   <TableCell></TableCell>
                   <TableCell></TableCell>
                   <TableCell></TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {filteredArray.map((tl, index) => (
+                {sortedData.map((tl, index) => (
                   <TableRow key={index}>
                     <TableCell width={'25%'}>
                       <Typography
