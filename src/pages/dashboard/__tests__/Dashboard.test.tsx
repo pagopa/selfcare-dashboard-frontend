210c8a77180a8eee08fa3d490daf005b39436fb9
esignore <elena.signore@nttdata.com>
2023-08-25 17:56:23 +0200

    SELC-2789 - removed test

--- src/pages/dashboard/__tests__/Dashboard.test.tsx
+++ src/pages/dashboard/__tests__/Dashboard.test.tsx
@@ -46,9 +46,6 @@ test('Test routing', async () => {
   history.push('/dashboard/13/prId/users');
   expect(history.location.pathname).toBe('/dashboard/13/prId/users');
 
-  history.push('/dashboard/7/ptPage');
-  await waitFor(() => expect(history.location.pathname).toBe('/dashboard/7/ptPage'));
-
   history.push('/dashboard/3/delegates');
   expect(history.location.pathname).toBe('/dashboard/3/delegates');
 
