8864aaa8aca901d5ae1125c7ef13b029a23ecabd
Oraldo Doci <oraldo.doci@nttdata.com>
2023-09-12 16:24:02 +0200

    SELC-2849_disable the reset filters button

--- src/pages/dashboardTechnologyPartnerPage/DashboardTablePT.tsx
+++ src/pages/dashboardTechnologyPartnerPage/DashboardTablePT.tsx
@@ -139,7 +139,7 @@ export default function DashboardTablePT({ filteredArray }: Props) {
           <Button
             variant="outlined"
             onClick={handleSearch}
-            disabled={!hasBeenDelegated || !searchTerm}
+            disabled={!hasBeenDelegated}
             size="small"
           >
             {t('overview.ptPage.filterTechPartner.buttonLabel')}
@@ -150,7 +150,7 @@ export default function DashboardTablePT({ filteredArray }: Props) {
             onClick={handleResetFilter}
             color="primary"
             variant="text"
-            disabled={!hasBeenDelegated || !searchTerm}
+            disabled={!hasBeenDelegated || (!searchTerm && selectedProduct === 'All')}
             sx={{ textAlign: 'center' }}
             component="button"
           >
