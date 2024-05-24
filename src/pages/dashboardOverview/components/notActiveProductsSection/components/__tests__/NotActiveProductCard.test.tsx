709b041bde80c28f4c6133387138907c6d632466
OraldoDoci <oraldo.doci@nttdata.com>
2024-05-24 10:03:04 +0200

    update unit tests

--- src/pages/dashboardOverview/components/notActiveProductsSection/components/__tests__/NotActiveProductCard.test.tsx
+++ src/pages/dashboardOverview/components/notActiveProductsSection/components/__tests__/NotActiveProductCard.test.tsx
@@ -80,7 +80,7 @@ describe('test onboarding', () => {
     fireEvent.click(button);
 
     expect(mockedLocation.assign).toBeCalledWith(
-      `http://selfcare/onboarding/${mockedProduct.id}?partyExternalId=${mockedParties[0].externalId}`
+      `http://selfcare/onboarding/${mockedProduct.id}?partyId=${mockedParties[0].partyId}`
     );
   });
 
