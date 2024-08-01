6edadb1654203443938646d5a5c4e45238d53f2d
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2024-08-01 15:09:51 +0200

    [SELC-5345] feat: Add Unit Tests for dashboard

---
+++ src/pages/dashboardOverview/components/activeProductsSection/components/__test__/GenericEnvProductModal.test.tsx
@@ -0,0 +1,77 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import GenericEnvProductModal from '../GenericEnvProductModal'; // Adjust the import as needed
+
+const mockProps = {
+  open: true,
+  title: 'Test Title',
+  message: 'Test Message',
+  handleClose: jest.fn(),
+  t: (key: string) => key,
+  productEnvironments: [
+    { environment: 'Collaudo', url: 'http://example.com/collaudo' },
+    { environment: 'Locale', url: 'http://example.com/locale' },
+  ],
+};
+
+test('renders and selects environment radio buttons correctly', () => {
+  render(<GenericEnvProductModal {...mockProps} />);
+
+  const collaudoRadio = screen.getByLabelText(/Ambiente di Collaudo/i);
+  const localeRadio = screen.getByLabelText(/Ambiente Locale/i);
+
+  expect(collaudoRadio).toBeInTheDocument();
+  expect(localeRadio).toBeInTheDocument();
+
+  // Simulate selection
+  fireEvent.click(collaudoRadio);
+  expect(collaudoRadio).toBeChecked();
+  expect(localeRadio).not.toBeChecked();
+
+  fireEvent.click(localeRadio);
+  expect(localeRadio).toBeChecked();
+  expect(collaudoRadio).not.toBeChecked();
+});
+
+test('enviroment collaudo', () => {
+  const handleClose = jest.fn();
+  const handleConfirm = jest.fn();
+
+  const mockProps = {
+    open: true,
+    title: 'Test Title',
+    message: 'Test Message',
+    handleClose,
+    onConfirm: handleConfirm,
+    onConfirmLabel: 'Entra',
+    productEnvironments: [{ environment: 'Collaudo', url: 'http://example.com/collaudo' }],
+    t: (key: string) => key,
+  };
+
+  render(<GenericEnvProductModal {...mockProps} />);
+
+
+  const closeButton = screen.getByRole('button', { name: /Annulla/i });
+  fireEvent.click(closeButton);
+  expect(handleClose).toHaveBeenCalled();
+});
+
+test('enviroment attestazione', () => {
+  const handleClose = jest.fn();
+  const handleConfirm = jest.fn();
+
+  const mockProps = {
+    open: true,
+    title: 'Test Title',
+    message: 'Test Message',
+    handleClose,
+    onConfirm: handleConfirm,
+    onConfirmLabel: 'Entra',
+    productEnvironments: [{ environment: 'Attestazione', url: 'http://example.com/attestazione' }],
+    t: (key: string) => key,
+  };
+
+  render(<GenericEnvProductModal {...mockProps} />);
+
+  const confirmButton = screen.getByRole('button', { name: /Entra/i });
+  fireEvent.click(confirmButton);
+});
