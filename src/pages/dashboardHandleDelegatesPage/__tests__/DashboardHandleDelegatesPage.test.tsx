eb4a4961a775d077e02a5ce84dbcc370021ca5fc
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2024-11-05 15:05:12 +0100

    unit test for on click of ea type delegationtest

--- src/pages/dashboardHandleDelegatesPage/__tests__/DashboardHandleDelegatesPage.test.tsx
+++ src/pages/dashboardHandleDelegatesPage/__tests__/DashboardHandleDelegatesPage.test.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 import { mockedParties } from '../../../services/__mocks__/partyService';
 import { renderWithProviders } from '../../../utils/test-utils';
 import DashboardHandleDelegatesPage from '../dashboardHandleDelegatesPage';
+import { fireEvent, screen } from '@testing-library/react';
 
 test('Should render component DashboardHandleDelegatesPage with populated props', async () => {
-  const mockedAgencyOnboarded = mockedParties[4];
-  renderWithProviders(<DashboardHandleDelegatesPage party={mockedAgencyOnboarded} />);
+  const mockedPartyRoma = mockedParties[2];
+  renderWithProviders(<DashboardHandleDelegatesPage party={mockedPartyRoma} />);
 });
 
+test('a party that is Aggregator can click on a delegated institution of type EA', async () => {
+  const mockedPartyAgencyOnboarded = mockedParties[4];
+  renderWithProviders(<DashboardHandleDelegatesPage party={mockedPartyAgencyOnboarded} />);
+
+  const delegatedInstitionName = await screen.findByText('Institution Name 0');
+  expect(delegatedInstitionName).toBeInTheDocument();
+  fireEvent.click(delegatedInstitionName);
+
+});
