36cc0f5effd566f15ee86849e9fe697bc5c6c803
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2024-11-05 15:16:26 +0100

    unit test

--- src/pages/dashboardHandleDelegatesPage/__tests__/DashboardHandleDelegatesPage.test.tsx
+++ src/pages/dashboardHandleDelegatesPage/__tests__/DashboardHandleDelegatesPage.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mockedParties } from '../../../services/__mocks__/partyService';
 import { renderWithProviders } from '../../../utils/test-utils';
-import DashboardHandleDelegatesPage from '../dashboardHandleDelegatesPage';
+import DashboardHandleDelegatesPage from '../DashboardHandleDelegatesPage';
 import { fireEvent, screen } from '@testing-library/react';
 
 test('Should render component DashboardHandleDelegatesPage with populated props', async () => {
