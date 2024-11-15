78c1429469f7be5b48f33d284cbf9c1712e0d5ed
loremedda <lorenzo.medda@nttdata.com>
2024-11-15 14:45:56 +0100

    [SELC-5986] feat: Removed italian fallback and using the translations provided for each language

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,8 +1,8 @@
 import i18n, { configureI18n } from '@pagopa/selfcare-common-frontend/lib/locale/locale-utils';
 import it from './it';
-import en from './it';
-import fr from './it';
-import sl from './it';
-import de from './it';
+import en from './en';
+import fr from './fr';
+import sl from './sl';
+import de from './de';
 
 configureI18n({ i18n, it, en, fr, sl, de });
