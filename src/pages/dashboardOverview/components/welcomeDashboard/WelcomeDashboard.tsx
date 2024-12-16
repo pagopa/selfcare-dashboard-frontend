42ed5efdc9baf6ce0e0ad267780799704e7b12a4
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2024-12-16 10:56:24 +0100

    remove not used var

--- src/pages/dashboardOverview/components/welcomeDashboard/WelcomeDashboard.tsx
+++ src/pages/dashboardOverview/components/welcomeDashboard/WelcomeDashboard.tsx
@@ -58,7 +58,7 @@ export default function WelcomeDashboard({ setOpen, party }: Readonly<Props>) {
           </Box>
         </Grid>
       </Grid>
-      {ENV.ENABLE_MOBILE_PHONE && party.userRole === 'ADMIN' && (
+      {party.userRole === 'ADMIN' && (
         <Grid item xs={12}>
           <Alert
             sx={{ mt: 5 }}
