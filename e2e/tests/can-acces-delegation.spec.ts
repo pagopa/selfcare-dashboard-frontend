4a8c51bf2813a794b1b24c943c3a868059e90830
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2025-03-04 09:58:48 +0100

    page route method

--- e2e/tests/can-acces-delegation.spec.ts
+++ e2e/tests/can-acces-delegation.spec.ts
@@ -9,5 +9,34 @@ test('instituion type pa onboarded on prod-pagopa can delegate', async ({ page }
   await page.getByRole('button', { name: 'Deleghe' }).click();
   await expect(page.getByRole('main')).toContainText('Delegato');
   await expect(page.getByRole('main')).toContainText('Comune di Frosinone');
-  await expect(page.getByRole('main')).toContainText('Qui trovi l’elenco degli enti a cui hai affidato la gestione di uno o più prodotti. Puoi anche aggiungere una nuova delega.');
+  await expect(page.getByRole('main')).toContainText(
+    'Qui trovi l’elenco degli enti a cui hai affidato la gestione di uno o più prodotti. Puoi anche aggiungere una nuova delega.'
+  );
+});
+
+test('get instituions 404', async ({ page }) => {
+  await login(page, 'p.rossi', 'test');
+  await page.route(
+    'https://api.dev.selfcare.pagopa.it/dashboard/v2/institutions',
+    async (route) => {
+      await route.fulfill({
+        status: 404,
+        contentType: 'application/json',
+        body: JSON.stringify({
+          detail: 'string',
+          instance: 'string',
+          invalidParams: [
+            {
+              name: 'string',
+              reason: 'string',
+            },
+          ],
+          status: 404,
+          title: 'string',
+          type: 'string',
+        }),
+      });
+    }
+  );
+
 });
