9f45f50aeeda2fbd8bdd306a2e2dff3ca3dc7ad7
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2025-10-09 12:52:29 +0200

    conditional skip parties call

--- src/decorators/withParties.tsx
+++ src/decorators/withParties.tsx
@@ -1,4 +1,6 @@
 import withRetrievedValue from '@pagopa/selfcare-common-frontend/lib/decorators/withRetrievedValue';
+import { storageUserOps } from '@pagopa/selfcare-common-frontend/lib/utils/storage';
+import React from 'react';
 import { useParties } from '../hooks/useParties';
 import { BaseParty } from '../model/Party';
 
@@ -9,5 +11,10 @@ export type WithPartiesProps = {
 export default function withParties<T extends WithPartiesProps>(
   WrappedComponent: React.ComponentType<T>
 ): React.ComponentType<Omit<T, 'parties' | 'reload'>> {
+  const loggedUser = storageUserOps.read();
+  const skipParties = loggedUser?.iss === 'PAGOPA';
+  if (skipParties) {
+    return WrappedComponent as React.ComponentType<Omit<T, 'parties' | 'reload'>>;
+  }
   return withRetrievedValue('parties', useParties, WrappedComponent);
 }
