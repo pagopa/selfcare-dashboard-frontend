214b95ce822de9f7981c4733495f55982b5a6b05
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2026-02-02 17:54:47 +0100

    [SELC-8202] feat: update copy for dora banner

--- src/pages/dashboardOverview/components/welcomeDashboard/WelcomeDashboard.tsx
+++ src/pages/dashboardOverview/components/welcomeDashboard/WelcomeDashboard.tsx
@@ -2,6 +2,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import { Box, Button, Grid } from '@mui/material';
 import TitleBox from '@pagopa/selfcare-common-frontend/lib/components/TitleBox';
 import { useTranslation } from 'react-i18next';
+import { isPagoPaUser } from '@pagopa/selfcare-common-frontend/lib/utils/storage';
 
 type Props = {
   setOpen: (open: boolean) => void;
@@ -13,43 +14,41 @@ export default function WelcomeDashboard({ setOpen }: Readonly<Props>) {
   const subTitle = t('overview.subTitle');
 
   return (
-
-      <Grid
-        container
-        spacing={2}
-        flexWrap={{ xs: 'wrap', md: 'nowrap' }}
-        justifyContent={'space-between'}
-      >
-        <Grid item sm={12} md={8}>
-          <TitleBox
-            title={title}
-            subTitle={subTitle}
-            mbTitle={2}
-            variantTitle="h4"
-            variantSubTitle="body1"
-          />
-        </Grid>
-        <Grid item justifyContent={'end'} sm={12} md={4} textAlign={{ xs: 'left', md: 'end' }}>
-          <Box>
-            <Button
-              variant="outlined"
-              onClick={() => setOpen(true)}
-              startIcon={<EditIcon />}
-              size="small"
-              sx={{
-                color: 'primary.main',
-                textWrap: 'no-wrap',
-                flexWrap: 'nowrap',
-                border: '2px solid primary.main',
-                fontWeight: '700px',
-                fontSize: '16px',
-              }}
-            >
-              {t('overview.changeDetails')}
-            </Button>
-          </Box>
-        </Grid>
+    <Grid
+      container
+      spacing={2}
+      flexWrap={{ xs: 'wrap', md: 'nowrap' }}
+      justifyContent={'space-between'}
+    >
+      <Grid item sm={12} md={8}>
+        <TitleBox
+          title={title}
+          subTitle={subTitle}
+          mbTitle={2}
+          variantTitle="h4"
+          variantSubTitle="body1"
+        />
       </Grid>
-
+      <Grid item justifyContent={'end'} sm={12} md={4} textAlign={{ xs: 'left', md: 'end' }}>
+        <Box>
+          <Button
+            variant="outlined"
+            onClick={() => setOpen(true)}
+            startIcon={<EditIcon />}
+            size="small"
+            sx={{
+              color: 'primary.main',
+              textWrap: 'no-wrap',
+              flexWrap: 'nowrap',
+              border: '2px solid primary.main',
+              fontWeight: '700px',
+              fontSize: '16px',
+            }}
+          >
+            {isPagoPaUser ? t('overview.viewDetails') : t('overview.changeDetails')}
+          </Button>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
