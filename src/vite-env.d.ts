76676b3b9711e116e061f01329fe155721d0cd68
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2026-04-02 17:21:17 +0200

    tests

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,12 +2,11 @@
 /// <reference types="vitest/globals" />
 
 interface ImportMetaEnv {
-  readonly VITE_ALLOWED_PRODUCTS_BACKSTAGE: string;
   readonly NODE_ENV: 'development' | 'uat' | 'production';
   readonly VITE_API_MOCK_REQUEST_DATA: string;
-  readonly NODE_ENV: 'development' | 'uat' | 'production';
-  readonly VITE_API_MOCK_PARTIES: string;
-  readonly VITE_API_MOCK_PRODUCTS: string;
+  readonly VITE_ALLOWED_PRODUCTS_BACKSTAGE: string;
+  VITE_API_MOCK_PARTIES: string;
+  VITE_API_MOCK_PRODUCTS: string;
 }
 
 interface ImportMeta {
