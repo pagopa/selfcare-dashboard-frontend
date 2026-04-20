40c98e41bf40dcf2ab4dba996ba5b5e75430c8f5
OraldoDoci <oraldo.doci@nttdata.com>
2026-04-20 18:05:18 +0200

    update onboardings path

--- src/pages/dashboard/components/dashboardSideMenu/hooks/useDashboardMenuItems.ts
+++ src/pages/dashboard/components/dashboardSideMenu/hooks/useDashboardMenuItems.ts
@@ -193,11 +193,11 @@ export function useDashboardMenuItems({
       key: 'institutionOnboardings',
       title: t('overview.sideMenu.institutionManagement.institutionOnboardings.title'),
       icon: DnsOutlinedIcon,
-      path: ENV.ROUTES.ADMIN_INSTITUTION_ONBOARDINGS,
+      path: ENV.ROUTES.ADMIN_ONBOARDINGS,
       isVisible: true,
-      isSelected: location.pathname.startsWith(ENV.ROUTES.ADMIN_INSTITUTION_ONBOARDINGS),
+      isSelected: location.pathname.startsWith(ENV.ROUTES.ADMIN_ONBOARDINGS),
       action: () => {
-        navigateTo(ENV.ROUTES.ADMIN_INSTITUTION_ONBOARDINGS);
+        navigateTo(ENV.ROUTES.ADMIN_ONBOARDINGS);
       },
     },
     {
